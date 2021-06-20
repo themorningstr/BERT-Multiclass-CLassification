2d16bbef626757b76d06f6a7a4e3d79cac103dcd
VISHWASH THAKUR <vishwasthakur35@gmail.com>
2021-06-20 18:48:57 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -31,8 +31,9 @@ def eval_fn(data_loader,model,device):
         Batch_input_id = data[0].to(device)
         Batch_attention_mask = data[1].to(device)
         Batch_target_label = data[2].to(device)
-        _,loss, = model(input_id = Batch_input_id,
-                        attention_mask = Batch_attention_mask,
-                        target_label = Batch_target_label)
+        with torch.no_grad()
+            _,loss, = model(input_id = Batch_input_id,
+                            attention_mask = Batch_attention_mask,
+                            target_label = Batch_target_label)
         final_loss += loss.item()
     return final_loss/len(data_loader)
